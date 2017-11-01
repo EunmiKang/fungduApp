fe2f8de35cd0beaf243592a23293d74dabe189e8
EunmiKang <hv19962000@gmail.com>
2017-11-01 15:45:19 +0900

    JOIN_ACTIVITY 생성

--- Biocube/app/src/main/java/com/example/seongjun/biocube/LoginActivity.java
+++ Biocube/app/src/main/java/com/example/seongjun/biocube/LoginActivity.java
@@ -1,9 +1,29 @@
 package com.example.seongjun.biocube;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by Seongjun on 2017. 11. 1..
  */
 
-public class LoginActivity{
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+
+    public void loginProcess(View view) {
+
+    }
+
+    public void goJoin(View view) {
+        Intent intent = new Intent(LoginActivity.this, JoinActivity.class);
+        startActivity(intent);
+    }
 
 }
