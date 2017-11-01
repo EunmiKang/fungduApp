9ef569b91d39d432d6bb5cf0c6b352817341b7ab
Seongjun2 <tjdwns4302@naver.com>
2017-11-01 15:49:35 +0900

    loginActvity imagview 추

--- Biocube/app/src/main/java/com/example/seongjun/biocube/LoginActivity.java
+++ Biocube/app/src/main/java/com/example/seongjun/biocube/LoginActivity.java
@@ -1,9 +1,19 @@
 package com.example.seongjun.biocube;
 
+import android.os.Bundle;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
 /**
  * Created by Seongjun on 2017. 11. 1..
  */
 
-public class LoginActivity{
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
 
+    }
 }
