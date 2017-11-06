dea16b9e4b6fdef9aa9da9647feb611eb5d3c645
Seongjun2 <tjdwns4302@naver.com>
2017-11-06 16:13:19 +0900

    fragment_user_manual.xml 틀작업 (fragment가 안보이는 문제 고쳐야 함)

--- Biocube/app/src/main/java/com/example/seongjun/biocube/UserManualFragment.java
+++ Biocube/app/src/main/java/com/example/seongjun/biocube/UserManualFragment.java
@@ -17,7 +17,7 @@ import android.view.ViewGroup;
  * Use the {@link UserManualFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class UserManualFragment extends Fragment {
+public class UserManualFragment extends Fragment  {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
