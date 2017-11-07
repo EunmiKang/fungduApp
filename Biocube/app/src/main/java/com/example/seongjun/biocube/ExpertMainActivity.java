2480b3c3fb35f3dc9ab1f4eac1fb23287bb09c34
EunmiKang <hv19962000@gmail.com>
2017-11-07 14:15:39 +0900

    일단 로그인하면 전문가 메인으로 넘어가게 하고 ExpertMainActivity 수정

--- Biocube/app/src/main/java/com/example/seongjun/biocube/ExpertMainActivity.java
+++ Biocube/app/src/main/java/com/example/seongjun/biocube/ExpertMainActivity.java
@@ -1,5 +1,7 @@
 package com.example.seongjun.biocube;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,16 @@ public class ExpertMainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_expert_main);
+
+        /* 뷰페이저 연결 */
+        ExpertPagerAdapter mExpertPagerAdapter = new ExpertPagerAdapter(
+                getSupportFragmentManager()
+        );
+        ViewPager mViewPager = (ViewPager) findViewById(R.id.viewpager);
+        mViewPager.setAdapter(mExpertPagerAdapter);
+
+        /* 탭 설정 */
+        TabLayout mTab = (TabLayout) findViewById(R.id.tabs);
+        mTab.setupWithViewPager(mViewPager);
     }
 }
