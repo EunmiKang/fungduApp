12f4288f10061dc76bbb575f890e2a337b3622d8
Seongjun2 <tjdwns4302@naver.com>
2017-11-07 15:48:28 +0900

    myCube 틀작업, titlebar 지움

--- Biocube/app/src/main/java/com/example/seongjun/biocube/CubeRegister.java
+++ Biocube/app/src/main/java/com/example/seongjun/biocube/CubeRegister.java
@@ -1,8 +1,19 @@
 package com.example.seongjun.biocube;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
 /**
  * Created by Seongjun on 2017. 11. 7..
  */
 
-public class CubeRegister {
+public class CubeRegister extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resgist_cube);
+    }
+
 }
