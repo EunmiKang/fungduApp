87f561ea3e4935957e9561b719b803f51b474a75
Seongjun2 <tjdwns4302@naver.com>
2017-11-16 15:38:11 +0900

    join onClick 추가(구현X) 코드 pull 위한 커밋.

--- Biocube/app/src/main/java/com/example/seongjun/biocube/JoinActivity.java
+++ Biocube/app/src/main/java/com/example/seongjun/biocube/JoinActivity.java
@@ -3,6 +3,8 @@ package com.example.seongjun.biocube;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
@@ -13,14 +15,22 @@ public class JoinActivity extends AppCompatActivity {
     //getCheckedRadioButtonId() 의 리턴값은 선택된 RadioButton 의 id 값.
     RadioButton rb = (RadioButton) findViewById(id);
 
+    Button.OnClickListener joinClickListener = new Button.OnClickListener() {
+        public void onClick(View v) {
+
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
 
+        findViewById(R.id.btn_join).setOnClickListener(joinClickListener);
         }
 
     public void joinProcess(View view) {
+
 
     }
 }
