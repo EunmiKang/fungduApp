0e51485624dfe55a2d0d360e2ff0ed16f9515cfc
Seongjun2 <tjdwns4302@naver.com>
2017-11-16 15:20:08 +0900

    radio 버튼 추가 joinActivity radio 선택 가져옴.

--- Biocube/app/src/main/java/com/example/seongjun/biocube/JoinActivity.java
+++ Biocube/app/src/main/java/com/example/seongjun/biocube/JoinActivity.java
@@ -3,14 +3,22 @@ package com.example.seongjun.biocube;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 public class JoinActivity extends AppCompatActivity {
+
+    final RadioGroup rg = (RadioGroup)findViewById(R.id.group_authority);
+    int id = rg.getCheckedRadioButtonId();
+    //getCheckedRadioButtonId() 의 리턴값은 선택된 RadioButton 의 id 값.
+    RadioButton rb = (RadioButton) findViewById(id);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
-    }
+
+        }
 
     public void joinProcess(View view) {
 
