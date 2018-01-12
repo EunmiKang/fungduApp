1884d37016c0eb7e31bc58baef4bd76adbcbb739
EunmiKang <hv19962000@gmail.com>
2018-01-12 17:39:16 +0900

    매뉴얼 등록 액티비티 수정중

--- Biocube/app/src/main/java/com/example/seongjun/biocube/ManualRegistActivity.java
+++ Biocube/app/src/main/java/com/example/seongjun/biocube/ManualRegistActivity.java
@@ -2,12 +2,23 @@ package com.example.seongjun.biocube;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class ManualRegistActivity extends AppCompatActivity {
+
+    EditText text_plantName;
+    Button btn_selectImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manual_regist);
+
+        text_plantName = (EditText) findViewById(R.id.text_manualRegist_plantName);
+        btn_selectImage = (Button) findViewById(R.id.btn_selectManualIamge);
+        
     }
+
+
 }
