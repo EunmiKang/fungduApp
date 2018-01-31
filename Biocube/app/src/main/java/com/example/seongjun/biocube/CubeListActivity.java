34612a18cff54d7cd26df77a26e142806b1d2d85
Seongjun2 <tjdwns4302@naver.com>
2018-01-31 20:12:15 +0900

    큐브 롱클릭시 다이얼로그 띄우고 삭제

--- Biocube/app/src/main/java/com/example/seongjun/biocube/CubeListActivity.java
+++ Biocube/app/src/main/java/com/example/seongjun/biocube/CubeListActivity.java
@@ -1,9 +1,11 @@
 package com.example.seongjun.biocube;
 
 import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.graphics.Color;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
@@ -12,6 +14,7 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
@@ -35,6 +38,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.concurrent.ExecutionException;
 
+import static android.R.attr.color;
+import static android.R.attr.dial;
 import static android.R.attr.id;
 
 /**
@@ -42,7 +47,7 @@ import static android.R.attr.id;
  */
 
 public class CubeListActivity extends AppCompatActivity {
-    String id;
+    String user_id;
     ListView list_cube;
     String[] cubeList;
     ArrayAdapter adapter;
@@ -55,10 +60,10 @@ public class CubeListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_cube_list);
 
         list_cube = (ListView) findViewById(R.id.list_cube);
-        id = getIntent().getStringExtra("id");
+        user_id = getIntent().getStringExtra("id");
 
         try {
-            String[] getList = new ReturnCubeList().execute(id).get();
+            String[] getList = new ReturnCubeList().execute(user_id).get();
             cubeList = new String[getList.length-1];
             for(int i=0; i<getList.length-1; i++) {
                 cubeList[i] = getList[i+1];
@@ -71,20 +76,116 @@ public class CubeListActivity extends AppCompatActivity {
             e.printStackTrace();
         }
 
-        list_cube.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView parent, View v, int position, long id) {
+        if(!user_id.equals("admin")) {
+            list_cube.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                @Override
+                public void onItemClick(AdapterView parent, View v, int position, long id) {
+                    String cubename = (String) parent.getItemAtPosition(position);
+                    Intent intent;
+                    intent = new Intent(CubeListActivity.this, DiaryAsCubeName.class);
+                    intent.putExtra("cubename", cubename);
+                    startActivity(intent);
 
-                // get TextView's Text.
-                String cubename = (String) parent.getItemAtPosition(position) ;
-                Intent intent;
-                intent = new Intent(CubeListActivity.this, DiaryAsCubeName.class);
-                intent.putExtra("cubename",cubename);
-                startActivity(intent);
+                    // TODO : use strText
+                }
+            });
 
-                // TODO : use strText
-            }
-        }) ;
+            list_cube.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+                @Override
+                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(CubeListActivity.this);
+                    final String cubename = (String) parent.getItemAtPosition(position);
+                    builder.setTitle("Cube 삭제 확인");
+                    builder.setMessage("해당 큐브를 삭제 하시겠습니까?");
+                    builder.setPositiveButton("삭제", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            new DeleteCube().execute(cubename);
+                        }
+                    });
+                    builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.cancel();
+                        }
+                    });
+                    AlertDialog dialog = builder.create();
+                    dialog.show();
+                    return true;
+                }
+            });
+        }
+        else{
+            list_cube.setClickable(false);
+            list_cube.setSelector(R.color.white);
+        }
     }
+    public class DeleteCube extends AsyncTask<Object,Object,Integer> {
+        // 실제 params 부분에는 execute 함수에서 넣은 인자 값이 들어 있다.
+        @Override
+        public Integer doInBackground(Object... params) {
+            try {
+             /* URL 설정하고 접속 */
+                URL url = new URL("http://fungdu0624.phps.kr/biocube/deletecube.php");
+                HttpURLConnection http = (HttpURLConnection) url.openConnection();
 
+            /* 전송모드 설정 */
+                http.setDefaultUseCaches(false);
+                http.setDoInput(true);  //서버에서 읽기 모드로 지정
+                http.setDoOutput(true);    //서버에서 쓰기 모드로 지정
+                http.setRequestMethod("POST");
+                http.setRequestProperty("content-type", "application/x-www-form-urlencoded");   //서버에게 웹에서 <Form>으로 값이 넘어온 것과 같은 방식으로 처리하라는 걸 알려준다
+
+            /* 서버로 값 전송 */
+                StringBuffer buffer = new StringBuffer();
+                buffer.append("user_id").append("=").append(user_id).append("&");
+                buffer.append("cubename").append("=").append(params[0].toString());
+                OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "EUC-KR");
+                PrintWriter writer = new PrintWriter(outStream);
+                writer.write(buffer.toString());
+                writer.flush();
+                writer.close();
+
+            /* 서버에서 전송 받기 */
+                InputStream inStream = http.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inStream, "UTF-8"));
+                String result = reader.readLine();
+
+                return Integer.parseInt(result);
+
+            } catch(MalformedURLException e) {
+                e.printStackTrace();
+            } catch(IOException e) {
+                e.printStackTrace();
+            }
+            //publishProgress(params);    // 중간 중간에 진행 상태 UI 를 업데이트 하기 위해 사용..
+            return -1;
+        }
+
+        @Override
+        public void onPostExecute(Integer result) {
+            super.onPostExecute(result);
+            if(result == 0 ){
+                Toast.makeText(CubeListActivity.this, "삭제 실패", Toast.LENGTH_SHORT).show();
+            }
+            else{
+                Toast.makeText(CubeListActivity.this, "삭제 성공", Toast.LENGTH_SHORT).show();
+                try {
+                    String[] getList = new ReturnCubeList().execute(user_id).get();
+                    cubeList = new String[getList.length-1];
+                    for(int i=0; i<getList.length-1; i++) {
+                        cubeList[i] = getList[i+1];
+                    }
+                    adapter = new ArrayAdapter(CubeListActivity.this, android.R.layout.simple_list_item_1, cubeList) ;
+                    list_cube.setAdapter(adapter);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } catch (ExecutionException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            // Todo: doInBackground() 메소드 작업 끝난 후 처리해야할 작업..
+        }
+    }
 }
--- Biocube/app/src/main/java/com/example/seongjun/biocube/CubeRegister.java
+++ Biocube/app/src/main/java/com/example/seongjun/biocube/CubeRegister.java
@@ -1,38 +1,28 @@
 package com.example.seongjun.biocube;
 
 import android.Manifest;
-import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.BroadcastReceiver;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.PackageManager;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
-import android.support.v4.view.LayoutInflaterCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.text.Layout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 import android.widget.TextView;
 import android.widget.Toast;
 
-
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
@@ -86,8 +76,6 @@ public class CubeRegister extends AppCompatActivity {
     String mStrDelimiter = "\n";
     char mCharDelimiter =  '\n';
     Thread mWorkerThread = null;
-    byte[] readBuffer;
-    int readBufferPosition;
     public static Context mcontext;
 //    CubeFragment mCubeFragment = new CubeFragment();
 
@@ -341,12 +329,4 @@ public class CubeRegister extends AppCompatActivity {
         mBluetoothAdapter.startDiscovery();
     }
 
-//    public void stopSocket(){
-//        try {
-//            mBluetooth.mSocket.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-    //새로운 소스적용 끝
 }
